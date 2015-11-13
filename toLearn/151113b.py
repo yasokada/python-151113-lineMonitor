a0dc96f7768099187131e0f6a8cd0ce8e844f292
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-13 12:53:26 +0900

    import 3 functions

---
+++ toLearn/151113b.py
@@ -0,0 +1,6 @@
+from util1113b import util1113_print_pi, util1113_print_napier, util1113_print_eulerMascheroni
+
+print "main"
+util1113_print_pi()
+util1113_print_napier()
+util1113_print_eulerMascheroni()
---
+++ toLearn/util1113b.py
@@ -0,0 +1,8 @@
+def util1113_print_pi():
+	print "3.141592"
+
+def util1113_print_napier():
+	print "2.718281828"
+
+def util1113_print_eulerMascheroni():
+	print "0.5772"
