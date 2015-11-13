3675b8b3c7e12ebd0989f831e21047cd50572b40
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-13 21:24:27 +0900

    WIP

--- utilUdpCmd.py
+++ utilUdpCmd.py
@@ -2,6 +2,18 @@
 import time
 import socket
 
+def procSetCommand(cmd1, cmd2, cmd3):
+	return
+
+def procCommand(rcvstr):
+	print "rcvd:", rcvstr
+	cmds = rcvstr.split(",")
+	if "set" in cmds[0]:
+		count = len(cmds)
+		if count == 4:
+			procSetCommand(cmds[1],cmds[2],cmds[3])	
+	return
+
 def recvCommand(cmdsock, rcvcmd):
 	try:
 		data,address = cmdsock.recvfrom(100)
@@ -24,7 +36,7 @@ def main():
 	while 1:
 		rcvcmd,rcvd = recvCommand(cmdsock, rcvcmd)
 		if rcvd == True and "\n" in rcvcmd:
-			print "rcvd command=",rcvcmd
+			procCommand(rcvcmd)
 			rcvcmd = ""
 
 if __name__ == '__main__':
