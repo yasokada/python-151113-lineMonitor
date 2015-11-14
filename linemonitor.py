7272abd4e8ded4e49e6bcdbe1c9bb1097b524fd1
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-14 09:02:38 +0900

    update todo

--- linemonitor.py
+++ linemonitor.py
@@ -2,16 +2,19 @@ from utilUdpCmd import recvCommand, procCommand
 from utilSetting import CSetting
 import socket
 
+# TODO: 1> something does not sit right concerning setting passing (g_setting)
+
 g_setting = CSetting()
 
 def main():
 	cmdip = ""
+	g_setting = CSetting()
 	cmdport = g_setting.getCmdPort()
 	cmdsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	cmdsock.bind((cmdip, cmdport))
 	cmdsock.setblocking(0)
 
-#	print "cmdport=", cmdport
+	print "cmdport=", cmdport
 
 	rcvcmd = ""
 
