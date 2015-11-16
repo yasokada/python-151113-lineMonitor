64da37ce02e66c4972d345f6b1f7c29cad3c0c79
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-16 21:22:27 +0900

    stop symbolic link

--- dummySerial.py
+++ dummySerial.py
@@ -1,1 +1,39 @@
-toLearn/dummySerial.py
+
+'''
+v0.1 2015/11/13
+  - add dummySerial.py
+'''
+
+class CDummySerial:
+	def __init__(self):
+		# do nothing
+		return
+
+	def read(self):
+		# do nothing
+#		print "read:" # TODO: 0 > remove
+		return ""
+
+	def write(self,txstr):
+		# do nothing
+#		print "write:", txstr # TODO: 0 > remove
+		return ""
+
+	@staticmethod
+	def Serial(name, baud, timeout):
+		work = CDummySerial()
+		return work
+
+# Usage
+
+'''
+from dummySerial import CDummySerial
+
+serial = CDummySerial()
+
+con1 = serial.Serial('/dev/ttyUSB1', 9600, 0.1)
+res = con1.read()
+con1.write("test")
+'''
+
+
