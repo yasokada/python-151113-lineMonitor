b2adb1cdc254c19616bc3e095b069d797384a6d3
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-25 07:14:47 +0900

    get last of ipadr and format to 00x

---
+++ toLearn/151125c.py
@@ -0,0 +1,22 @@
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+def IPADR_getLastDigits(ipadr):
+	ipadrs = ipadr.split(".")
+	return ipadrs[3]
+
+ipadr = get_ip_address('eth0')  # '192.168.xxx.xxx'
+print ipadr
+
+last = IPADR_getLastDigits(ipadr)
+print last
+
