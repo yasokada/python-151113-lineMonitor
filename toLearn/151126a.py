889757c4f1a318b0d9a44551cf262506758e1da0
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-26 07:05:35 +0900

    2 dimension array snippet

---
+++ toLearn/151126a.py
@@ -0,0 +1,17 @@
+onoff=[
+[3, 1, 4, 1, 5],
+[2, 7, 1, 8, 2],
+[0, 5, 7, 7, 2],
+]
+
+print onoff[0][0],
+print onoff[0][1],
+print onoff[0][2]
+
+print onoff[1][0],
+print onoff[1][1],
+print onoff[1][2]
+
+print onoff[2][0],
+print onoff[2][1],
+print onoff[2][2]
