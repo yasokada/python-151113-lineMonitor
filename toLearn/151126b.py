83318a9bd530a783c33c434d27199cdca81e816b
Yasuhiko OKADA <yasokada@gmail.com>
2015-11-26 21:15:59 +0900

    prepare all onoff for 0..9

--- toLearn/151126b.py
+++ toLearn/151126b.py
@@ -10,11 +10,11 @@ onoff=[
 [True,  True, True,  True, False,      False, True, False], # disp 3
 [False, True, True, False, False,      False, True,  False ], # disp 4
 [True, False, True, True, False,       True,  True, False], # disp 5
-# TODO: 1> disp 2..9
-[False, False, False, False, False,   False, False, False], # dummy 6
-[False, False, False, False, False,   False, False, False], # dummy 7
-[False, False, False, False, False,   False, False, False], # dummy 8
-[False, False, False, False, False,   False, False, False], # dummy 9
+[True, False, True, True,  True,       True, True, False], # disp 6
+[True, True, True, False, False,       True, False, False], # disp 7
+[True, True, True,  True,  True,       True, True, False], # disp 8
+[True, True, True,  True,  False,       True, True, False], # disp 9
+[False, False, False, False, False,     False, False, True], # disp .
 ]
 # TODO: 0z > add out of range display
 #-------------------
@@ -26,4 +26,4 @@ for digit in range(0, 4): # TODO: 1> size of
 	code = codes[digit]
 	print code
 	for idx in range(0, numsegs):
-		print pinnum[idx], onoff[code- 1][idx]
+		print pinnum[idx], onoff[code][idx]
